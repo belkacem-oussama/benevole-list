f060af1ae98db896fa82fe1bbe9b6073fd49b1af
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-17 15:41:36 +0200

    create first-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import "./App.css"
+import Home from "./pages/Home.jsx"
 
 function App() {
-  return "Hello"
+  return <Home />
 }
 
 export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Liste des bénévoles.</h1>
+    </div>
+  )
+}
