64da5024cd762896612e767c5793bf5bb7c9b3c9
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-17 17:15:49 +0200

    reorganise

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -7,18 +7,18 @@ export default function Grid() {
             <input type="checkbox" name="" id="" />
           </th>
           <th>Identité</th>
-          <th>Phone</th>
-          <th>Mail</th>
-          <th>Supprimer</th>
-          <th>Modifier</th>
+          <th>Téléphone</th>
+          <th>Ajouté(e) le :</th>
+          <th></th>
+          <th></th>
         </tr>
         <tr>
           <td>
             <input type="checkbox" name="" id="" />
           </td>
-          <td>Identité</td>
+          <td>John Doe John-Doe@unknown.com</td>
           <td>06.06.06.06.06</td>
-          <td>John-Doe@unknown.com</td>
+          <td>Le 28 Septembre 2023</td>
           <td>Supprimer</td>
           <td>Modifier</td>
         </tr>
