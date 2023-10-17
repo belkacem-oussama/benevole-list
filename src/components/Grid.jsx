a81bf5d219806dd254d68513e57c57f90f97a47a
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-17 16:02:34 +0200

    structure table

---
+++ src/components/Grid.jsx
@@ -0,0 +1,24 @@
+export default function Grid() {
+  return (
+    <div className="grid-container">
+      <table>
+        <tr>
+          <th>Select</th>
+          <th>Identité</th>
+          <th>Phone</th>
+          <th>Mail</th>
+          <th>Supprimer</th>
+          <th>Modifier</th>
+        </tr>
+        <tr>
+          <td>Select</td>
+          <td>Identité</td>
+          <td>06.06.06.06.06</td>
+          <td>John-Doe@unknown.com</td>
+          <td>Supprimer</td>
+          <td>Modifier</td>
+        </tr>
+      </table>
+    </div>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import Grid from "../components/Grid.jsx"
+
 export default function Home() {
   return (
     <div className="home-container">
       <h1>Liste des bénévoles.</h1>
       <p>Séléctionnez et gérez les membres de votre équipe.</p>
+      <Grid />
     </div>
   )
 }
