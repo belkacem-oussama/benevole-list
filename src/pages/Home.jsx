b134a55dfc54a8d047fbdceb27a21feb0591b859
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-18 13:12:17 +0200

    remove title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,6 @@ import Grid from "../components/Grid.jsx"
 export default function Home() {
   return (
     <div className="home-container">
-      <h1>Liste des bénévoles.</h1>
-      <p>Séléctionnez et gérez les membres de votre équipe.</p>
       <Grid />
     </div>
   )
