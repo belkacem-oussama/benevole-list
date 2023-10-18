4ddb5d99a2412d00e9df2eccd91abaa0a3f8c374
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-18 10:59:50 +0200

    remove an item on click

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import monkey_d_luffy_img from "../assets/img/luffy.png"
 import luke_skywalker_img from "../assets/img/luke-skywalker.webp"
 import harry_potter_img from "../assets/img/harry-potter.jpeg"
@@ -12,6 +14,20 @@ import katness_everdeen_img from "../assets/img/katness-everdeen.jpeg"
 import users from "../assets/json/user.json"
 
 export default function Grid() {
+  const [removedItems, setRemovedItems] = useState([])
+
+  const handleToggleRemove = (userId) => {
+    if (removedItems.includes(userId)) {
+      setRemovedItems((prevItems) =>
+        prevItems.filter((item) => item !== userId)
+      )
+    } else {
+      setRemovedItems((prevItems) => [...prevItems, userId])
+    }
+  }
+
+  const isRemoved = (userId) => removedItems.includes(userId)
+
   const infosIMG = {
     monkey_d_luffy_img: monkey_d_luffy_img,
     harry_potter_img: harry_potter_img,
@@ -42,7 +58,10 @@ export default function Grid() {
         </thead>
         <tbody>
           {users.characters.map((user) => (
-            <tr key={user.id}>
+            <tr
+              key={user.id}
+              className={isRemoved(user.id) ? "removed_item" : ""}
+            >
               <td>
                 <input type="checkbox" name="" id="" />
               </td>
@@ -59,25 +78,38 @@ export default function Grid() {
               </td>
               <td>{user.phone}</td>
               <td>{user.created_at}</td>
-              <td>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="26"
-                  height="26"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7 4V2H17V4H22V6H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V6H2V4H7ZM6 6V20H18V6H6ZM9 9H11V17H9V9ZM13 9H15V17H13V9Z"></path>
-                </svg>
+              <td onClick={() => handleToggleRemove(user.id)}>
+                {isRemoved(user.id) ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="26"
+                    height="26"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M10.0007 15.1709L19.1931 5.97852L20.6073 7.39273L10.0007 17.9993L3.63672 11.6354L5.05093 10.2212L10.0007 15.1709Z"></path>
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="26"
+                    height="26"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M7 4V2H17V4H22V6H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V6H2V4H7ZM6 6V20H18V6H6ZM9 9H11V17H9V9ZM13 9H15V17H13V9Z"></path>
+                  </svg>
+                )}
               </td>
               <td>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="26"
-                  height="26"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M15.7279 9.57629L14.3137 8.16207L5 17.4758V18.89H6.41421L15.7279 9.57629ZM17.1421 8.16207L18.5563 6.74786L17.1421 5.33365L15.7279 6.74786L17.1421 8.16207ZM7.24264 20.89H3V16.6474L16.435 3.21233C16.8256 2.8218 17.4587 2.8218 17.8492 3.21233L20.6777 6.04075C21.0682 6.43128 21.0682 7.06444 20.6777 7.45497L7.24264 20.89Z"></path>
-                </svg>
+                {!isRemoved(user.id) && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="26"
+                    height="26"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M15.7279 9.57629L14.3137 8.16207L5 17.4758V18.89H6.41421L15.7279 9.57629ZM17.1421 8.16207L18.5563 6.74786L17.1421 5.33365L15.7279 6.74786L17.1421 8.16207ZM7.24264 20.89H3V16.6474L16.435 3.21233C16.8256 2.8218 17.4587 2.8218 17.8492 3.21233L20.6777 6.04075C21.0682 6.43128 21.0682 7.06444 20.6777 7.45497L7.24264 20.89Z"></path>
+                  </svg>
+                )}
               </td>
             </tr>
           ))}
