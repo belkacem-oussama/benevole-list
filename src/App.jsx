ce96172f5d99a1a1544833032b86cd8b9fd6afca
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-18 20:46:29 +0200

    add new pathh

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import Home from "./pages/Home.jsx"
 import "../src/assets/styles/import.scss"
 import "remixicon/fonts/remixicon.css"
 import { Route, Routes } from "react-router-dom"
+import Edit from "./pages/Edit.jsx"
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/edit" element={<Edit />} />
       </Routes>
     </>
   )
