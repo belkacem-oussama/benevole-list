aec0153b5f2915c123f324e06c76816298d757c3
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-18 20:45:59 +0200

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import Home from "./pages/Home.jsx"
 import "../src/assets/styles/import.scss"
 import "remixicon/fonts/remixicon.css"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
-  return <Home />
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App
