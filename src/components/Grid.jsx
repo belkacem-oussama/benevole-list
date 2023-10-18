5c52be266782724c6dc29f3c4a6ff49c8c629190
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2023-10-18 10:13:22 +0200

    import img

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,7 +1,30 @@
-import img from "../assets/img/luffy.png"
+import monkey_d_luffy_img from "../assets/img/luffy.png"
+import luke_skywalker_img from "../assets/img/luke-skywalker.webp"
+import harry_potter_img from "../assets/img/harry-potter.jpeg"
+import hermione_granger_img from "../assets/img/hermione-granger.jpeg"
+import aragorn_img from "../assets/img/aragorn.jpeg"
+import sherlock_holmes_img from "../assets/img/sherlock-holmes.jpeg"
+import jon_snow_img from "../assets/img/jon-snow.webp"
+import arya_stark_img from "../assets/img/aria-stark.webp"
+import gandalf_img from "../assets/img/gandalf.webp"
+import katness_everdeen_img from "../assets/img/katness-everdeen.jpeg"
+
 import users from "../assets/json/user.json"
 
 export default function Grid() {
+  const infosIMG = {
+    monkey_d_luffy_img: monkey_d_luffy_img,
+    harry_potter_img: harry_potter_img,
+    luke_skywalker_img: luke_skywalker_img,
+    hermione_granger_img: hermione_granger_img,
+    aragorn_img: aragorn_img,
+    sherlock_holmes_img: sherlock_holmes_img,
+    jon_snow_img: jon_snow_img,
+    arya_stark_img: arya_stark_img,
+    gandalf_img: gandalf_img,
+    katness_everdeen_img: katness_everdeen_img,
+  }
+
   return (
     <div className="grid-container">
       <table>
@@ -25,7 +48,7 @@ export default function Grid() {
               </td>
               <td>
                 <span className="first-td">
-                  <img src={img} alt="" />
+                  <img src="" alt="" />
                   <ul>
                     <li>
                       {user.name} - <span>{user.occupation}</span>
